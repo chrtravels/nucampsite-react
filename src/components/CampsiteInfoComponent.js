ab310fd6a8f16730341702b9dc647ef224f35b88
Chris Reynoso <chrtravels@gmail.com>
2020-11-04 04:35:10 -0800

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -23,7 +23,7 @@ function RenderCampsite({campsite}) {
   )
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -36,7 +36,7 @@ function RenderComments({comments, addComment, campsiteId}) {
               {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
             </p> 
             </div>)}
-            <CommentForm campsiteId={campsiteId} addComment={addComment} />
+            <CommentForm campsiteId={campsiteId} postComment={postComment} />
       </div>
     );
   } return <div></div>;
@@ -71,7 +71,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
       this.toggleModal();
-      this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+      this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
 
   render() {
@@ -167,7 +167,7 @@ function CampsiteInfo(props) {
         <RenderCampsite campsite={props.campsite} />
         <RenderComments 
             comments={props.comments}
-            addComment={props.addComment}
+            postComment={props.postComment}
             campsiteId={props.campsite.id}
         />
       </div>
